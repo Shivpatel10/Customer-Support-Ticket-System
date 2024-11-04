df1d25ba04f812ab8e1b23637f218143570d807a
Shiv Patel <shivpatel0204@gmail.com>
2024-11-03 21:17:33 -0500

    Implement async/await to fetch ticket data and handle errors.

--- main.js
+++ main.js
@@ -2,7 +2,24 @@
 
 // Task 2: Fetch Tickets Using Async/Await and Handle Errors
 async function fetchUnresolvedTicketsAPI() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const customer = await response.json();
-    console.log('Customer:', customer);
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts'); //// Fetch data from the API
+
+        
+        if (!response.ok) { //using if to see response success
+            throw new Error('Problem fetching tickets.');
+        }
+        const ticketsUnresolved = await response.json(); //// Parse the JSON data
+
+        if (ticketsUnresolved.length === 0) { // If tickets found = 0 then, error
+            throw new Error('No unresolved tickets found.');
+        }
+        displayTickets(ticketsUnresolved); // calls displaytickets so it can display when the function is used
+
+    } catch (error) {
+        // Display the error message in the error message section
+        document.getElementById('errorMessage').textContent = error.message;
+        console.error('Error:', error.message);
+    }
 }
+fetchUnresolvedTicketsAPI(); // Call the function to fetch tickets
