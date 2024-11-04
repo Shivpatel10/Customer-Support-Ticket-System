aa22f4b3102f9b2f5770105da954d2735acd069c
Shiv Patel <shivpatel0204@gmail.com>
2024-11-03 19:58:48 -0500

    Setup basic HTML structure for the ticket system.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 // Task 1: Setup HTML Structure for the Ticket System
 
 // Task 2: Fetch Tickets Using Async/Await and Handle Errors
+async function fetchUnresolvedTicketsAPI() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const customer = await response.json();
+    console.log('Customer:', customer);
+}
